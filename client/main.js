1a1d69a9d712be2e339a23ad20dbec27ebb3b103
Fabio Z Tessitore <fabio@localhost.localdomain>
2015-05-24 14:47:55 +0200

    aggiunto il colore rosso al modello di linee

--- client/main.js
+++ client/main.js
@@ -4,7 +4,10 @@ Template.lines.helpers({
       y1: 0,
       x2: 0,
       y2: 100,
-      w: 2
+      w: 2,
+      color: {
+        r: 255
+      }
     },
     {
       x1: 0,
@@ -12,6 +15,9 @@ Template.lines.helpers({
       x2: 104,
       y2: 100,
       w: 4,
+      color: {
+        r: 200
+      }
     },
     {
       x1: 100,
@@ -19,13 +25,19 @@ Template.lines.helpers({
       x2: 100,
       y2: 0,
       w: 8,
+      color: {
+        r: 150,
+      }
     },
     {
       x1: 100,
       y1: 0,
       x2: 0,
       y2: 0,
-      w: 16
+      w: 16,
+      color: {
+        r: 100
+      }
     }
   ]
 });
